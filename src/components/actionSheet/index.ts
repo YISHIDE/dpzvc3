9944b44bf3b87ae2fdc37ff1c199b1dc177eba24
[dingpingzhong] <[804741102@qq.com]>
2026-01-05 13:08:44 +0800

    修改actionSheet组件类型声明

--- src/components/actionSheet/index.ts
+++ src/components/actionSheet/index.ts
@@ -1,6 +1,13 @@
 /**
  * Created by admin on 2025/9/19.
  */
+import type { App } from 'vue'
 import ActionSheet from './actionSheet'
-export default ActionSheet
+ActionSheet.install = (app: App): void => {
+    app.component('ActionSheet', ActionSheet); // 注册全局组件
+}
+type IWithInstall<T> = T & { install(app: App): void };
+const _ActionSheet=ActionSheet as IWithInstall<typeof ActionSheet>;
+export default _ActionSheet
+// export default ActionSheet
 export type { ActionSheetProps } from './types'
