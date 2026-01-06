9ca601d92054be2bf640a38d9c5bcc2b8cbf804a
[dingpingzhong] <[804741102@qq.com]>
2026-01-07 04:03:12 +0800

    更改渲染

--- server.js
+++ server.js
@@ -1,20 +1,54 @@
+// /* eslint-disable */
+// const express = require('express')
+// const fs = require('fs')
+// const path = require('path')
+// process.env.SSR = 'true'
+// // CommonJS 服务端 bundle 导入
+// const pkg = require('./dist-ssr/server/entry-server.js')
+// const render = pkg.render
+
+// const server = express()
+// // 静态资源
+// server.use(express.static(path.resolve(__dirname, 'dist-ssr/client')))
+
+// // 所有请求走 SSR
+// server.get('/.*', async (req, res) => {
+//   try {
+//     const template = fs.readFileSync(path.resolve(__dirname, 'dist-ssr/client/index.html'), 'utf-8')
+//     const appHtml = await render(req.url)
+//     const html = template.replace('<!--app-html-->', appHtml)
+//     res.setHeader('Content-Type', 'text/html')
+//     res.end(html)
+//   } catch (err) {
+//     console.error(err)
+//     res.status(500).send('SSR Error')
+//   }
+// })
+
+// server.listen(8000, () => {
+//   console.log('SSR server running at http://localhost:8000')
+// })
 /* eslint-disable */
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+
 process.env.SSR = 'true'
+
 // CommonJS 服务端 bundle 导入
 const pkg = require('./dist-ssr/server/entry-server.js')
 const render = pkg.render
 
 const server = express()
+
 // 静态资源
 server.use(express.static(path.resolve(__dirname, 'dist-ssr/client')))
 
-// 所有请求走 SSR
-server.get('/.*', async (req, res) => {
+// 所有请求走 SSR（兼容 Vercel）
+server.use(async (req, res, next) => {
   try {
-    const template = fs.readFileSync(path.resolve(__dirname, 'dist-ssr/client/index.html'), 'utf-8')
+    const templatePath = path.resolve(__dirname, 'dist-ssr/client/index.html')
+    const template = fs.readFileSync(templatePath, 'utf-8')
     const appHtml = await render(req.url)
     const html = template.replace('<!--app-html-->', appHtml)
     res.setHeader('Content-Type', 'text/html')
@@ -25,6 +59,7 @@ server.get('/.*', async (req, res) => {
   }
 })
 
-server.listen(8000, () => {
-  console.log('SSR server running at http://localhost:8000')
+const port = process.env.PORT || 8000
+server.listen(port, () => {
+  console.log(`SSR server running at http://localhost:${port}`)
 })
